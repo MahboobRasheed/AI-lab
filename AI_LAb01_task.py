92883d6e7a2790e689491dd6dd61a025e0670af7
Mahboob Rasheed <mahboobrasheed50@gmail.com>
2025-09-06 03:53:38 -0700

    Create AI_LAb01_task.py

---
+++ AI_LAb01_task.py
@@ -0,0 +1,75 @@
+#Arithmetic operators
+
+a= int(input("enter your first number:"))
+b= int(input("enter your first number:"))
+print("Addition:", a + b)
+a= int(input("enter your first number:"))
+b= int(input("enter your first number:"))
+print("Subtraction:", a - b)
+a= int(input("enter your first number:"))
+b= int(input("enter your first number:"))
+print("Multiplication:", a * b)
+a= int(input("enter your first number:"))
+b= int(input("enter your first number:"))
+print("Division:", a / b)
+a= int(input("enter your first number:"))
+b= int(input("enter your first number:"))
+print("Floor Division:", a // b)
+a= int(input("enter your first number:"))
+b= int(input("enter your first number:"))
+print("Modulus:", a % b)
+a= int(input("enter your first number:"))
+b= int(input("enter your first number:"))
+print("Exponent:", a ** b)
+
+#Assignment Operators
+
+x = 5
+print("Initial x:", x)
+
+x += 3
+print("After x += 3:", x)
+
+x -= 2
+print("After x -= 2:", x)
+
+x *= 4
+print("After x *= 4:", x)
+
+x /= 3
+print("After x /= 3:", x)
+
+x %= 5
+print("After x %= 5:", x)
+
+x **= 2
+print("After x **= 2:", x)
+
+#Comparison Operators
+p= int(input("enter your first number:"))
+q= int(input("enter your first number:"))
+print("p == q:", p == q)
+p= int(input("enter your first number:"))
+q= int(input("enter your first number:"))
+print("p != q:", p != q)
+p= int(input("enter your first number:"))
+q= int(input("enter your first number:"))
+print("p > q:", p > q)
+p= int(input("enter your first number:"))
+q= int(input("enter your first number:"))
+print("p < q:", p < q)
+p= int(input("enter your first number:"))
+q= int(input("enter your first number:"))
+print("p >= q:", p >= q)
+p= int(input("enter your first number:"))
+q= int(input("enter your first number:"))
+print("p <= q:", p <= q)
+
+# Logical Operators
+a = True
+b = False
+
+print("a and b:", a and b)
+print("a or b:", a or b)
+print("not a:", not a)
+print("not b:", not b)
